1cb838097149d5ef570265f490aebb90c1d80abd
Xyphrs <dromerof01@elpuig.xeill.net>
2021-03-08 18:30:03 +0100

    Proves Unitaries

---
+++ src/ex2/HashTableTest2.java
@@ -0,0 +1,52 @@
+package ex2;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+class HashTableTest2 {
+
+    @ParameterizedTest
+    @CsvSource({"5,5", "10,10", "30,30"})
+    void put(String key, String value) {
+        ex2.HashTable hashTable = new ex2.HashTable();
+        hashTable.put(key, value);
+        Assertions.assertNotNull(hashTable.get(key));
+    }
+
+    @ParameterizedTest
+    @CsvSource({"5", "10", "20"})
+    void get(String key) {
+        ex2.HashTable hashTable = new ex2.HashTable();
+        for(int i=0; i<30; i++) {
+            final String keys = String.valueOf(i);
+            hashTable.put(keys, keys);
+        }
+        Assertions.assertEquals(key, hashTable.get(key));
+    }
+
+    @ParameterizedTest
+    @CsvSource({"5", "10", "20"})
+    void drop(String key) {
+        ex2.HashTable hashTable = new ex2.HashTable();
+        hashTable.put(key,key);
+        hashTable.drop(key);
+        Assertions.assertEquals(0, hashTable.count());
+        Assertions.assertNull(hashTable.get(key));
+    }
+
+    @org.junit.jupiter.api.Test
+    void count() {
+        ex2.HashTable hashTable = new ex2.HashTable();
+        hashTable.put("5","5");
+        Assertions.assertEquals(1, hashTable.count());
+        hashTable.drop("5");
+        Assertions.assertEquals(0, hashTable.count());
+    }
+
+    @org.junit.jupiter.api.Test
+    void size() {
+        ex2.HashTable hashTable = new HashTable();
+        Assertions.assertEquals(16, hashTable.size());
+    }
+}
